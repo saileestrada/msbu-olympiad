fbf9a8e3e25b5d8182ab863d2342779d9074f740
saileestrada <saileestrada1691@gmail.com>
2026-02-11 18:11:14 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Dashboard from './Dashboard'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
